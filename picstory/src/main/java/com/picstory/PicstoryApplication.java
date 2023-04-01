bd5a88c34bfc445ea7313429a310ba3203a3aea2
JG서 <sjk1052005@gmail.com>
2023-04-01 23:45:22 +0900

    [ADD] #0 - Picstory Server Project

---
+++ picstory/src/main/java/com/picstory/PicstoryApplication.java
@@ -0,0 +1,13 @@
+package com.picstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicstoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicstoryApplication.class, args);
+    }
+
+}
---
+++ picstory/src/test/java/com/picstory/PicstoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicstoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
